9282aba634c5b046e25b88ced53cb28f163508b3
zokratez <tortillabar@me.com>
2016-05-04 02:17:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+#Tuno = Dog.new
+#Tuno.name = "Tuno"
+#puts Tuno.name
